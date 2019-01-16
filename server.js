073492e187ad0d66ee31b175851252f4b711e29a
Computer <computer@Computers-MacBook-Pro.local>
2019-01-16 15:06:11 -0700

    added show route

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const app = express();
 
 const Pokemon = require('./models/pokemon');
 
-app.get('/pokemon', (req, res) => {
-    res.send(Pokemon);
-})
+// set up your data base
+// app.get('/pokemon', (req, res) => {
+//     res.send(Pokemon);
+// })
 
+// set up your index view
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {
+        pokemonList: Pokemon,
+    });
+});
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemon: Pokemon[req.params.id]
+    });
+});
 
 app.listen(3000, () => {
     console.log('pokemon server working')
-})
+});
