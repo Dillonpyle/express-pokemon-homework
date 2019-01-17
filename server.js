51bb353e438f25ba15baf09dd529fe9f446cba54
Computer <computer@Computers-MacBook-Pro.local>
2019-01-16 17:22:02 -0700

    done

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-
 const Pokemon = require('./models/pokemon');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+//middleware
+app.use(methodOverride('_method'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // set up your data base
 // app.get('/pokemon', (req, res) => {
@@ -15,13 +23,45 @@ app.get('/pokemon', (req, res) => {
     });
 });
 
+//create route
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+app.post('/pokemon', (req, res) => {
+    Pokemon.push(req.body);
+    console.log(req.body)
+    res.redirect('/pokemon');
+})
+
 // show route
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
-        pokemon: Pokemon[req.params.id]
+        pokemon: Pokemon[req.params.id],
+        index: req.params.id
     });
 });
 
+//edit route
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('edit.ejs', {
+        pokemonEdit: Pokemon[req.params.id],
+        index: req.params.id
+    });
+});
+app.put('/pokemon/:index', (req, res) => {
+    Pokemon[req.params.index] = req.body;
+    res.redirect('/pokemon');
+
+});
+
+//delete route
+app.delete('/pokemon/:id', (req, res) => {
+    Pokemon.splice(req.params.id, 1);
+    res.redirect('/pokemon');
+})
+
+//server
 app.listen(3000, () => {
     console.log('pokemon server working')
 });
